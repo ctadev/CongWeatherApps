9fec927516e626fc3f607cea0ca557e6d9fa47b4
ctadev <53548168+ctadev@users.noreply.github.com>
2021-12-15 21:15:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.scss";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [datas, setDatas] = useState();
+  const [city, setCity] = useState();
+
+  const url = `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=${city}&aqi=no`;
+
+  const fetchData = async () => {
+    try {
+      await fetch(url).then((response) =>
+        response.json().then((data) => setDatas(data))
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const cityHandler = (e) => {
+    setCity(e.target.value);
+  };
+
+  const KeyPress = (event) => {
+    if (event.key == "Enter") {
+      fetchData();
+    }
+  };
+
+  if (datas) {
+    return (
+      <section className="app-container">
+        <div className="header-container">
+          <h1>Cong's Weather App</h1>
+          <input
+            value={city}
+            placeholder="Enter City..."
+            onChange={cityHandler}
+            type="text"
+            onKeyPress={KeyPress}
+          />
+          <button onClick={() => fetchData()}>Search</button>
+        </div>
+        {typeof datas.location === "undefined" ? (
+          <div className="no-city">
+            <p>
+              Welcome to Cong's Weather App, Enter a city to get the weather.
+            </p>
+          </div>
+        ) : (
+          <div>
+            <div className="location-container">
+              <h2>{`${datas.location.name}, ${datas.location.region}`}</h2>
+              <h2>{datas.location.country}</h2>
+            </div>
+
+            <div className="temp-container">
+              <img src={datas.current.condition.icon} />
+              <p>{`${datas.current.temp_c}°C`}</p>
+            </div>
+          </div>
+        )}
+      </section>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Loading...Api fetching....</h1>
     </div>
   );
 }
